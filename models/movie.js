140c4b8196aac61264ef8db1ddf5062ea779b8e0
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-08-06 00:31:03 +0700

    feat: реализовать схемы сущностей БД

---
+++ models/movie.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const { URL_REGEX } = require('../utils/constants');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => URL_REGEX.test(url),
+      message: 'Необходимо ввести URL',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => URL_REGEX.test(url),
+      message: 'Необходимо ввести URL',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => URL_REGEX.test(url),
+      message: 'Необходимо ввести URL',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', movieSchema);
