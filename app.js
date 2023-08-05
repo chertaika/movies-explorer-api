0b88de092b6c909daf1e6413ff5b64d92540f82b
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-08-06 00:07:44 +0700

    feat: настроить подключение к БД

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { DB_URI, PORT } = require('./utils/config');
 
 const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(DB_URI);
+    console.log('Соединение с базой данных установлено');
+  } catch (error) {
+    console.log(`Ошибка соединения с базой данных ${error.message}`);
+  }
+})();
+
+app.listen(PORT, () => {
+  console.log(`Приложение слушает порт ${PORT}`);
+});
